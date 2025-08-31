5b6f7f83539f46dc11b910e846929e3fc5973500
laiscasagrande <laisluanaka@outlook.com>
2025-08-31 15:47:33 -0300

    finalização da resolução da questão número 03

--- src/main/java/ex03/Main.java
+++ src/main/java/ex03/Main.java
@@ -1,26 +1,51 @@
 package ex03;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Playlist playlist = new Playlist();
-        Musica musica = new Musica();
         MusicCloud musicaCloud = new MusicCloud();
+        int parar = 0;
+
+        //Músicas que existem no cloud(nuvem):
+        Musica musica01 = new Musica(); //cada objeto que representa uma música precisa ser criado, não podemos aproveitar o mesmo objeto para músicas diferentes
+        musica01.titulo = "333";
+        musica01.artista = "Matuê";
+
+        Musica musica02 = new Musica();
+        musica02.titulo = "Crazy Love";
+        musica02.artista = "Kant";
+
+        musicaCloud.musicasDisponiveis.add(musica01);
+        musicaCloud.musicasDisponiveis.add(musica02);
 
         System.out.print("Digite o nome da playlist: ");
         playlist.nome = scanner.nextLine();
 
-        System.out.print("Digite o nome da música: ");
-        musica.titulo = scanner.nextLine();
-        musicaCloud.musicasDisponiveis.add(musica);
-        musicaCloud.pesquisarMusica(musica.titulo);
+        while (parar == 0) {
+            System.out.print("Digite o nome da música: ");
+            String titulo = scanner.nextLine();
 
-        if (musica.titulo == musicaCloud) {
-            playlist.musicas.add(musica.titulo);
-        }else{
-            System.out.println("Esta música não encontra-se nas músicas disponíveis!");
+            Musica musicaEncontrada = musicaCloud.pesquisarMusica(titulo); //vamos passar o título da música digitada para o metodo pesquisarMusica para verificar se existe uma música com este título
+
+            if  (musicaEncontrada != null) {
+                playlist.musicas.add(musicaEncontrada);
+                System.out.println("Você adicionou a música " + musicaEncontrada.titulo + " na playlist.");
+            }else{
+                System.out.println("A música " + titulo + " não está disponível");
+            }
+
+            System.out.print("Deseja parar? Caso decida finalizar o processo, digite 1, e caso deseje continuar, digite 0: ");
+            parar = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("Playlist: " +  playlist.nome);
+        for (Musica musica : playlist.musicas){
+            System.out.println("- " + musica.titulo + " (" + musica.artista + ")");
         }
     }
  }
--- src/main/java/ex03/MusicCloud.java
+++ src/main/java/ex03/MusicCloud.java
@@ -3,9 +3,14 @@ package ex03;
 import java.util.ArrayList;
 
 public class MusicCloud {
-    ArrayList<Musica> musicasDisponiveis;
+    ArrayList<Musica> musicasDisponiveis = new ArrayList<>(); //array apenas para verificar se a música existe
 
-    void pesquisarMusica(String titulo) {
-        musicasDisponiveis = new ArrayList<>();
+    Musica pesquisarMusica(String titulo){
+        for (Musica musica : musicasDisponiveis){
+            if (musica.titulo.equals(titulo)){ //se o título da musica que esta dentro da lista musicasDisponiveis for igual ao titulo da musica que vou receber como parametro dessa função
+                return musica;
+            }
+        }
+        return null;
     }
 }
